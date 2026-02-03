4d151f922d7c34f0c05044718d1ffa949cac5e12
alekigsm <igr0k@yandex.ru>
2026-02-03 19:37:51 +0300

    добавил api генератор  (swagger), обновил зависимости

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,12 +2,38 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import 'dotenv/config';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
-  app.setGlobalPrefix('api/afisha');
+
   app.enableCors();
+  app.setGlobalPrefix('api/afisha');
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  // Настраиваем Swagger БЕЗ учета глобального префикса
+  const config = new DocumentBuilder()
+    .setTitle('Мой фильмапроект')
+    .setDescription('Практикум Films')
+    .setVersion('1.0')
+    .addServer('http://localhost:3000/') // Указываем полный путь с префиксом
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config, {
+    ignoreGlobalPrefix: true, // Игнорируем глобальный префикс для Swagger
+  });
+
+  // Swagger доступен по /api/docs (без префикса afisha)
+  SwaggerModule.setup('api/docs', app, document);
+
   await app.listen(3000);
+  console.log(`Application is running on: http://localhost:3000/api/afisha`);
+  console.log(`Swagger documentation: http://localhost:3000/api/docs`);
 }
 bootstrap();
