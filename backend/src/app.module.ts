7a3855906bd229afb7e9a5d00a65fa9ff227b8ce
alekigsm <igr0k@yandex.ru>
2026-02-03 20:30:40 +0300

    через NestJS модули

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,39 +2,22 @@ import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { ConfigModule } from '@nestjs/config';
 import * as path from 'node:path';
-import { MongooseModule } from '@nestjs/mongoose'; // импорт
-
+import { MongooseModule } from '@nestjs/mongoose';
 import { configProvider } from './app.config.provider';
-import { FilmsController } from './films/films.controller';
-import { OrderController } from './order/order.controller';
-import { OrderService } from './order/order.service';
-import { FilmsService } from './films/films.service';
-import { FilmsRepository } from './repository/films.repository';
-import { FilmSchema } from './films/schema/filmSchema';
-import { OrdersRepository } from './repository/order.repository';
+import { FilmsModule } from './films/films.module';
+import { OrderModule } from './order/order.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://127.0.0.1:27017/afisha'), // ← ДОБАВЬТЕ ЭТО!
-
-    ConfigModule.forRoot({
-      isGlobal: true,
-      cache: true,
-    }),
+    MongooseModule.forRoot('mongodb://127.0.0.1:27017/afisha'),
+    ConfigModule.forRoot({ isGlobal: true, cache: true }),
     ServeStaticModule.forRoot({
       rootPath: path.join(__dirname, '..', 'public'),
       serveRoot: '/content/afisha',
     }),
-
-    MongooseModule.forFeature([{ name: 'Film', schema: FilmSchema }]),
+    FilmsModule,
+    OrderModule,
   ],
-  controllers: [FilmsController, OrderController],
-  providers: [
-    configProvider,
-    OrderService,
-    FilmsService,
-    FilmsRepository,
-    OrdersRepository,
-  ],
+  providers: [configProvider],
 })
 export class AppModule {}
