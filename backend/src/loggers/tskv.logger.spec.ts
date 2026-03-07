3d37c71082cf6bf721a1e2dbfa61837db384adf7
alekigsm <igr0k@yandex.ru>
2026-03-07 16:03:47 +0300

    test

--- backend/src/loggers/tskv.logger.spec.ts
+++ backend/src/loggers/tskv.logger.spec.ts
@@ -1,7 +1,30 @@
 import { TskvLogger } from './tskv.logger';
 
 describe('TskvLogger', () => {
-  it('should be defined', () => {
-    expect(new TskvLogger()).toBeDefined();
+  let logger: TskvLogger;
+  let consoleSpy: jest.SpyInstance;
+
+  beforeEach(() => {
+    logger = new TskvLogger();
+
+    consoleSpy = jest.spyOn(console, 'log').mockImplementation();
+  });
+
+  afterEach(() => {
+    consoleSpy.mockRestore();
+  });
+
+  it('should format log message correctly', () => {
+    logger.log('тестовое сообщение', 'param1', 'param2');
+
+    expect(consoleSpy).toHaveBeenCalledTimes(1);
+
+    const output = consoleSpy.mock.calls[0][0];
+
+    // Проверяем формат
+    expect(output).toContain('level=log');
+    expect(output).toContain('message=тестовое сообщение');
+    expect(output).toContain('\t'); // табуляция
+    expect(output.endsWith('\n')).toBe(true);
   });
 });
